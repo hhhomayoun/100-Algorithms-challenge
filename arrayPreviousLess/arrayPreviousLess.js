b243371e7c781b18fc89680983573a9698b56b31
Homayoun Ostad <hhhomayoun@yahoo.com>
2020-12-12 13:17:40 -0800

    problem 17

---
+++ arrayPreviousLess/arrayPreviousLess.js
@@ -0,0 +1,16 @@
+function arrayPreviousLess(items) {
+ let result = [];
+ for (let i = items.length - 1; i >= 0; i--) {
+  for (let j = i; j >= 0; j--) {
+   if (items[i] > items[j]) {
+    result.unshift(items[j]);
+    break;
+   } else if (j === 0) {
+    result.unshift(-1);
+   }
+  }
+ }
+ return result;
+}
+
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
