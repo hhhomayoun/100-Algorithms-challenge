57a9c225335bc8afd4523437949750e59d59884a
Homayoun Ostad <hhhomayoun@yahoo.com>
2021-02-17 23:38:08 -0800

    problem 56

---
+++ firstDigit/firstDigit.ts
@@ -0,0 +1,12 @@
+function firstDigit(inputString: string): string {
+ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+ for (let i = 0; i < inputString.length; i++) {
+  if (digits.includes(inputString[i])) {
+   return inputString[i]
+  }
+ }
+}
+
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));
