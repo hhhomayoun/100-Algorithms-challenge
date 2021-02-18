3d7c6d35080b6c37d18934fbe21c0728c0137615
Homayoun Ostad <hhhomayoun@yahoo.com>
2021-02-18 13:43:07 -0800

    added tscconfig.josn file

--- firstDigit/firstDigit.js
+++ firstDigit/firstDigit.js
@@ -1,8 +1,9 @@
 function firstDigit(inputString) {
-    var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    for (var i = 0; i < inputString.length; i++) {
-        if (digits.includes(inputString[i])) {
-            return inputString[i];
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let chars = inputString.split('');
+    for (let i = 0; i < chars.length; i++) {
+        if (digits.includes(chars[i])) {
+            return chars[i];
         }
     }
 }
